8e9a6512173339b3f587e89f01c814fbe711eea7
illia <115975062+AquaCats@users.noreply.github.com>
2024-12-02 21:08:12 +0200

    Delete create_fund_p1 directory

--- create_fund_p1/create3.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('input[type="file"]').addEventListener('change', function () {
-  const fileName = this.files[0]?.name || "Немає вибраного файлу";
-  document.querySelector('.upload-box span').textContent = fileName;
-});
